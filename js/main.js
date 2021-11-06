261e3e4c2401ea305625541d169ee6e8207dc7f0
kriztofer3103 <81489983+kriztofer3103@users.noreply.github.com>
2021-11-05 21:59:38 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let fullImgBox = document.getElementById("fullImgBox");
 let fullImg = document.getElementById("fullImg");
+let referencia = [7];
 let leftArrow = document.getElementById("leftArrow");
 let rigthArrow = document.getElementById("rigthArrow");
 
@@ -7,50 +8,63 @@ const openFullImg = (reference) => {
     fullImgBox.classList.add("moveX");
     fullImg.src = reference;
 
-    var referencia = new Array(7);
-    for(i=0; i<8; i++){
+    for(let i=0; i<8; i++){
         referencia[i] = `http://127.0.0.1:5500/images/img-${i+1}.jpg`;
     }
 
-    if(reference == `http://127.0.0.1:5500/images/img-1.jpg`){
-        leftArrow.style.display = "none";
-        rigthArrow.style.display = "block";
+    if(reference == referencia[0]){
+        esconderFlechaIzquierda();
     }
 
-    else if(reference == `http://127.0.0.1:5500/images/img-8.jpg`){
-        leftArrow.style.display = "block";
-        rigthArrow.style.display = "none";
+    else if(reference == referencia[7]){
+        esconderFlechaDerecha();
     }
 
     else{
-        leftArrow.style.display = "block";
-        rigthArrow.style.display = "block";
+        mostrarFlechas();
     }
 
-    var i =0;
     leftArrow.addEventListener("click", function(){
-        i--;
-
-        fullImg.src = referencia [i];
-
-        if(fullImg.src == referencia[0]){
-            leftArrow.style.display = "none";
-            rigthArrow.style.display = "block";
+        for(let i=0; i<8; i++){
+            if(reference == referencia[0]){
+                esconderFlechaIzquierda();
+            }
+            else if(reference == referencia[i]){
+                fullImg.src = referencia[i-1];
+                reference = referencia[i-1];
+                mostrarFlechas();
+            }
         }
-        rigthArrow.style.display = "block";
+
     });
 
     rigthArrow.addEventListener("click", function(){
-        i++;
-
-        fullImg.src = referencia[i]; 
-
-        if(fullImg.src == referencia[7]){
-            leftArrow.style.display = "block";
-            rigthArrow.style.display = "none";
+        for(let i=7; i>=0; i--){
+            if(reference == referencia[7]){
+                esconderFlechaDerecha();
+            }
+            else if(reference == referencia[i]){
+                fullImg.src = referencia[i+1];
+                reference = referencia[i+1];
+                mostrarFlechas();
+            }
         }
-        leftArrow.style.display = "block";
     });
+}
+
+const esconderFlechaIzquierda = () =>{
+    leftArrow.style.display = "none";
+    rigthArrow.style.display = "block";
+}
+
+const esconderFlechaDerecha = () =>{
+    leftArrow.style.display = "block";
+    rigthArrow.style.display = "none";
+}
+
+const mostrarFlechas = () =>{
+    leftArrow.style.display = "block";
+    rigthArrow.style.display = "block";
 }
 
 const closeFullImg = () => {
